5584d88ad019bf82cd8df02cdcc320d0bab075fe
jainaman224 <jainaman224@gmail.com>
2015-07-23 15:57:18 +0530

    much better :-)

--- calc.js
+++ calc.js
@@ -1,27 +1,56 @@
+$('#e').hide();
+
+i = -1;
+str=[];
+str1=[];
+
 function c(val)
 {
+if (val == ""){i=0;}
 document.getElementById("d").value=val;
+document.getElementById("e").value=val;
 }
 
-function v(val)
+function v(val, valu)
 {
-document.getElementById("d").value+=val;
+i+= 1;
+str[i]=val;
+document.getElementById("d").value+=str[i];
+str1[i]=valu;
+document.getElementById("e").value+=str1[i];
 }
 
 function aman()
 {
-var str = $('#d').val();
-$('#d').val(str.substring(0, str.length - 1));
+if (i>0){
+i-=1;
+document.getElementById("d").value="";
+document.getElementById("e").value="";
+for (var j=0; j<=i; j+=1)
+document.getElementById("d").value+=str[j];
+document.getElementById("e").value+=str1[j];
+}
+}
+
+function pi()
+{
+var x = Math.PI;
+v('pi', x);
 }
 
 function e() 
 { 
 try 
 { 
-  c(eval(document.getElementById("d").value)) 
+  val=eval(document.getElementById("e").value);
+  i=0;
+  str[i]=val;
+  str1[i]=val;
+  c(val);
 } 
 catch(e) 
 {
-  c('Error') 
+  i=0;
+  c('Error');
 } 
 }  
