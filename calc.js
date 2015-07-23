28f1521be1419b3648417954bc79af83e48e976f
jainaman224 <jainaman224@gmail.com>
2015-07-23 14:01:03 +0530

    better

--- calc.js
+++ calc.js
@@ -8,6 +8,12 @@ function v(val)
 document.getElementById("d").value+=val;
 }
 
+function aman()
+{
+var str = $('#d').val();
+$('#d').val(str.substring(0, str.length - 1));
+}
+
 function e() 
 { 
 try 
